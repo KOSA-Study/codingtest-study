ba239f0cd0ea9352de7955a274a49c9800f35a7a
seongminQa <129333587+seongminQa@users.noreply.github.com>
2024-03-27 13:54:51 +0900

    [Bronze III 2562] Title: 최댓값, Time: 208 ms, Memory: 17680 KB -NKLCBHub

--- mingyeongwon/#2562 최댓값/최댓값.java
+++ mingyeongwon/#2562 최댓값/최댓값.java
@@ -1,33 +1,25 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    
-    int[] arr = new int[9];
-    
-    for(int i=0; i<9; i++) {
-      arr[i] = sc.nextInt();
-    }
-    
-    int max = 0;
-    int num = 0;
-    
-    for(int i=0; i<9; i++) {
-      if(arr[i] > max) {
-        max = arr[i];
-        num = i+1;
-      }
-    }
-    System.out.println(max);
-    System.out.println(num);
-    
-    
-    
-    
-    sc.close();
-
-  }
-
+	public static void main(String[] args) {
+		// 스캐너를 사용하여 입력받는다.
+		Scanner scan = new Scanner(System.in);
+		// 자연수 9개를 저장할 정수 배열 선언
+		int[] arry = new int[9];
+		// 최댓값을 비교하고 저장할 변수 선언
+		int maxNum = Integer.MIN_VALUE;
+		// 최댓값이 몇번째에 들어있는지 저장할 변수 선언
+		int index = 0;
+		// for문을 이용하여 9번 입력을 받고, 최댓값과 몇번째 입력값인지 판별한다.
+		for (int i = 0; i < arry.length; i++) {
+			arry[i] = scan.nextInt();
+			if (arry[i] > maxNum) {
+				maxNum = arry[i];
+				index = i + 1;
+			}
+		}
+		System.out.println(maxNum);
+		System.out.print(index);
+	}
 }
