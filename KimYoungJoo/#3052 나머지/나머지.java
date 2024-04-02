5785bcdd529bbfffb7bf818410a65477b786ab3b
GIM-YOUNG-JOO <162575148+GIM-YOUNG-JOO@users.noreply.github.com>
2024-04-02 21:49:55 +0900

    [Bronze II 3052] Title: 나머지, Time: 204 ms, Memory: 17760 KB -NKLCBHub

--- KimYoungJoo/#3052 나머지/나머지.java
+++ KimYoungJoo/#3052 나머지/나머지.java
@@ -5,9 +5,6 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		int[] arr = new int[10];
 		int remain = 0;
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = -1;
-		}
 
 		for (int i = 0; i < 10; i++) {
 			arr[i] = scan.nextInt() % 42;
