e14537a2c7f657508b65620802479eeb8d003c94
seongminQa <129333587+seongminQa@users.noreply.github.com>
2024-04-04 01:29:40 +0900

    [Bronze II 3052] Title: 나머지, Time: 76 ms, Memory: 11488 KB -NKLCBHub

--- seongminQa/#3052 나머지/나머지.java
+++ seongminQa/#3052 나머지/나머지.java
@@ -4,22 +4,24 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		// for문 안에 입력을 받기위해 BufferedReader 보조 스트림을 사용
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		// 처음 수를 입력받을 변수 선언
-		int[] arrayNum = new int[10];
-		// 입력받은 수에 42로 나눈 나머지를 저장할 변수 선언
-		int[] checkNum = new int[10];
-		// for문을 이용하여 10개의 입력을 받고 동시에 두 변수의 값을 대입
+		int[] arrayNum = new int[42];
+		// 카운트할 변수 선언
+		int cnt = 0;
+		// for문을 이용하여 10개의 입력을 받고 해당 인덱스에 1을 저장
 		for (int i = 0; i < 10; i++) {
-			arrayNum[i] = Integer.parseInt(br.readLine());
-			checkNum[i] = arrayNum[i] % 42;
+			arrayNum[Integer.parseInt(br.readLine()) % 42] = 1;
 		}
-		// Arrays클래스의 메소드를 사용하여 중복값 제거
-		int[] resultNum = Arrays.stream(checkNum).distinct().toArray();
-		System.out.println(resultNum.length);
+		// 해당 인덱스에 1이 저장되어 있다면 카운트
+		for(int i=0; i<arrayNum.length; i++) {
+			if(arrayNum[i] != 0) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
 	}
 }
