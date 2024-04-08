a6b5aa1aa80d054494b911ff53ecf16412c46e41
Seong min k <129333587+seongminQa@users.noreply.github.com>
2024-04-09 00:55:10 +0900

    [Bronze V 11654] Title: 아스키 코드, Time: 204 ms, Memory: 17620 KB -NKLCBHub

---
+++ seongminQa/#11654 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String value = scan.nextLine();
+		System.out.println((int)value.charAt(0));
+	}
+}
